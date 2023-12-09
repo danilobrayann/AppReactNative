fc7a942800e7c7b219d0ce3ce949ed8dd1ebe1a8
Danilo Souza <danilobrayann@gmail.com>
2023-12-08 21:13:58 -0300

    feat: criando estruturas em React Native

---
+++ App.js
@@ -0,0 +1,9 @@
+import Home from "./page/home";
+
+
+export default function App() {
+  return (
+    <Home /> 
+  );
+}
+
---
+++ page/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components/native";
+
+
+export const Container = styled.View`
+flex: 1;
+justify-content:center;
+align-items:center;
+background-color: black;
+height: 100vh;
+
+
+`
+
+export const Texto = styled.Text`
+color: red;
+font-size:25px;
+
+`
+
+export const Subtite  = styled.Text`
+color: white;
+`
